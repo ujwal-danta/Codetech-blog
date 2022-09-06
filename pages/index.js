0f1d142e910b1972f6eef0f938148598605d359a
ujwal-danta <ujwaldanta@gmail.com>
2022-09-06 20:15:25 +0530

    display post

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,30 @@ import Layout from "../components/Layout";
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
+import Link from 'next/link'
+import Post from "../components/Post";
 
 export default function HomePage({ posts }) {
-  console.log(posts)
   return (
     <Layout>
-      <h1>HELOO</h1>
+      <h1 className="text-5xl border-b-4 p-5 font-bold">Latest Posts</h1>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {posts.map((post, index) => {
+          return (
+            <Post key={index} post={post} />
+          )
+        })}
+      </div>
+      <Link href='/blog'>
+        <a className="block text-center border border-gray-500 text-gray-800 
+        rounded-md py-4 my-5 transition duration-500 ease select-none 
+        hover:text-white hover:bg-gray-900 focus:outline-none 
+        focus:shadow-outline w-full">
+          All Posts
+        </a>
+      </Link>
     </Layout>
+
   )
 }
 
