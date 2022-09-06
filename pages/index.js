dc48c162205cb62234d8a52aaeacd8377fc08d90
ujwal-danta <ujwaldanta@gmail.com>
2022-09-06 19:33:18 +0530

    get posts & parse frontmatter

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,37 @@
 import Layout from "../components/Layout";
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
 
-export default function HomePage() {
+export default function HomePage({ posts }) {
+  console.log(posts)
   return (
     <Layout>
       <h1>HELOO</h1>
     </Layout>
   )
+}
+
+
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join('posts'))
+  const posts = files.map(filename => {
+    const slug = filename.replace('.md', '')
+
+    const markdownWithMeta = fs.readFileSync(path.join('posts', filename), 'utf-8')
+    const { data: frontmatter } = matter(markdownWithMeta)
+
+    return {
+      slug,
+      frontmatter
+    }
+  })
+
+
+
+  return {
+    props: {
+      posts
+    }
+  }
 }
